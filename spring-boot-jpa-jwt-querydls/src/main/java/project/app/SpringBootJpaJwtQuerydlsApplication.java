be751eb5249f18afaa971295db1534466ae645e6
Igor <132262b@naver.com>
2023-03-03 23:41:07 +0900

    new project templates

---
+++ spring-boot-jpa-jwt-querydls/src/main/java/project/app/SpringBootJpaJwtQuerydlsApplication.java
@@ -0,0 +1,13 @@
+package project.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaJwtQuerydlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaJwtQuerydlsApplication.class, args);
+	}
+
+}
