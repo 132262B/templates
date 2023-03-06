8fc30117a9b4e4cda0f3c0916bbbe3f0e7461edf
Igor <132262b@naver.com>
2023-03-06 20:54:33 +0900

    util 추가

---
+++ spring-boot-jpa-jwt-querydls/src/main/java/com/app/global/util/UriUtil.java
@@ -0,0 +1,17 @@
+package com.app.global.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@UtilityClass
+public class UriUtil {
+
+    public static URI createUri(Long id) {
+        return ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
